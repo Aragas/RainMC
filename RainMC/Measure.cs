44e583b7f2f6975f20cbe986350e76f415e709a9
Vitalij <micvitalij@yandex.ru>
2014-01-12 23:39:49 +0400

    Night commit. GUI version.

--- RainMC/Measure.cs
+++ RainMC/Measure.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using MinecraftClient;
+using MinecraftClientGUI;
+using MinecraftClient = MinecraftClientGUI.MinecraftClient;
 
 namespace Rainmeter
 {
     /// <summary>
     /// Main part of Measure.
     /// </summary>
-    internal partial class Measure
+    internal class Measure
     {
         private static string Username = "";
         private static string Password = "";
         private static string ServerIP = "";
+
+        public static string Path = "";
+
+        private static MinecraftClient MC;
 
         internal enum MeasureType
         {
@@ -19,44 +24,12 @@ namespace Rainmeter
         }
         private MeasureType _type;
 
-        internal enum CountType
-        {
-            One,
-            Two,
-            Three,
-            Four,
-            Five,
-            Six,
-            Seven
-        }
-        private CountType _countType;
-
         /// <summary>
         /// Called when Rainmeter is launched. Just once.
         /// Is called before skin gets data.
         /// </summary>
         public Measure()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
-        }
-
-        private System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            string dllName = args.Name.Contains(",")
-                ? args.Name.Substring(0, args.Name.IndexOf(','))
-                : args.Name.Replace(".dll", "");
-
-            dllName = dllName.Replace(".", "_");
-
-            if (dllName.EndsWith("_resources")) return null;
-
-            System.Resources.ResourceManager rm =
-                new System.Resources.ResourceManager(GetType().Namespace + ".Properties.Resources",
-                    System.Reflection.Assembly.GetExecutingAssembly());
-
-            byte[] bytes = (byte[])rm.GetObject(dllName);
-
-            return System.Reflection.Assembly.Load(bytes);
         }
 
         /// <summary>
@@ -66,6 +39,13 @@ namespace Rainmeter
         /// <param name="api">Rainmeter API</param>
         internal void Initialize(Rainmeter.API api)
         {
+            if (String.IsNullOrEmpty(Path))
+            {
+                string path = api.ReadPath("Type", "");
+                if (!String.IsNullOrEmpty(path))
+                    Path = path.Replace("\\" + path.Split('\\')[7], "\\");
+            }
+
             string type = api.ReadString("Type", "");
 
             switch (type.ToUpperInvariant())
@@ -73,39 +53,6 @@ namespace Rainmeter
 
                 case "ANSWER":
                     _type = MeasureType.Answer;
-
-                    int counttype = api.ReadInt("Count", 1);
-                    switch (counttype)
-                    {
-                        case 1:
-                            _countType = CountType.One;
-                            break;
-
-                        case 2:
-                            _countType = CountType.Two;
-                            break;
-
-                        case 3:
-                            _countType = CountType.Three;
-                            break;
-
-                        case 4:
-                            _countType = CountType.Four;
-                            break;
-
-                        case 5:
-                            _countType = CountType.Five;
-                            break;
-
-                        case 6:
-                            _countType = CountType.Six;
-                            break;
-
-                        case 7:
-                            _countType = CountType.Seven;
-                            break;
-                    }
-
                     break;
 
                 case "LOGIN":
@@ -137,39 +84,6 @@ namespace Rainmeter
 
                 case "ANSWER":
                     _type = MeasureType.Answer;
-
-                    int counttype = api.ReadInt("Count", 1);
-                    switch (counttype)
-                    {
-                        case 1:
-                            _countType = CountType.One;
-                            break;
-
-                        case 2:
-                            _countType = CountType.Two;
-                            break;
-
-                        case 3:
-                            _countType = CountType.Three;
-                            break;
-
-                        case 4:
-                            _countType = CountType.Four;
-                            break;
-
-                        case 5:
-                            _countType = CountType.Five;
-                            break;
-
-                        case 6:
-                            _countType = CountType.Six;
-                            break;
-
-                        case 7:
-                            _countType = CountType.Seven;
-                            break;
-                    }
-
                     break;
 
                 case "LOGIN":
@@ -199,66 +113,11 @@ namespace Rainmeter
             switch (_type)
             {
                 case MeasureType.Answer:
-
-                    if (ConsoleIO.History.Last != null)
-                    {
-                        switch (_countType)
-                        {
-                            case CountType.One:
-                                return ConsoleIO.History.Last.Value;
-
-                            case CountType.Two:
-                                try
-                                {
-                                    return ConsoleIO.History.Last.Previous.Value;
-                                }
-                                catch {}
-                                break;
-
-                            case CountType.Three:
-                                try
-                                {
-                                    return ConsoleIO.History.Last.Previous.Previous.Value;
-                                }
-                                catch {}
-                                break;
-
-                            case CountType.Four:
-                                try
-                                {
-                                    return ConsoleIO.History.Last.Previous.Previous.Previous.Value;
-                                }
-                                catch {}
-                                break;
-
-                            case CountType.Five:
-                                try
-                                {
-                                    return ConsoleIO.History.Last.Previous.Previous.Previous.Previous.Value;
-                                }
-                                catch {}
-                                break;
-
-                            case CountType.Six:
-                                try
-                                {
-                                    return ConsoleIO.History.Last.Previous.Previous.Previous.Previous.Previous.Value;
-                                }
-                                catch {}
-                                break;
-
-                            case CountType.Seven:
-                                try
-                                {
-                                    return ConsoleIO.History.Last.Previous.Previous.Previous.Previous.Previous.Previous.Value;
-                                }
-                                catch {}
-                                break;
-                        }
-                    }
-
-                    if (ConsoleIO.History.Last != null)
-                        return ConsoleIO.History.Last.Value;
+                    if (MC != null)
+                        return MC.ReadLine();
+                    //switch (_countType)
+                    //{
+                    //}
                     break;
 
             }
@@ -273,24 +132,37 @@ namespace Rainmeter
         {
             if (command.ToUpperInvariant() == "START")
             {
-                MClient.Initialize(Username, Password, ServerIP);
+                if (MC == null)
+                    MC = new MinecraftClient(Username, Password, ServerIP);
             }
+                
             else if (command.ToUpperInvariant() == "RESTART")
             {
-                MClient.Restart();
+                if (MC != null)
+                {
+                    MC.Close();
+                    MC = new MinecraftClient(Username, Password, ServerIP);
+                }
             }
+
             else if (command.ToUpperInvariant() == "EXIT")
             {
-                MClient.Exit();
+                if (MC != null)
+                {
+                    MC.Close();
+                    MC = null;
+                }
             }
+
             else if (command.StartsWith("Text:"))
             {
-                ConsoleIO.Text = command.Substring(5);
+                if (MC != null)
+                    MC.SendText(command.Substring(5));
             }
+
             else
-            {
                 API.Log(API.LogType.Error, "RainMC.dll Command " + command + " not valid");
-            }
+
         }
 
         /// <summary>
